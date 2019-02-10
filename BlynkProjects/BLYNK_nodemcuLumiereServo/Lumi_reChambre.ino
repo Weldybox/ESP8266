4ada9ea500493f134c9461f9da60a69ee0b589d1
Weldybox <43091716+Weldybox@users.noreply.github.com>
2019-02-10 16:25:12 +0100

    Update Lumi_reChambre.ino

--- BlynkProjects/BLYNK_nodemcuLumiereServo/Lumi_reChambre.ino
+++ BlynkProjects/BLYNK_nodemcuLumiereServo/Lumi_reChambre.ino
@@ -4,12 +4,12 @@
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
-char auth[] = "a53d4bf5a8e14ef6aa4ae26330bc3405";
+char auth[] = "";
 int pos;
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "Livebox-8E6A";
-char pass[] = "EC6364F7327751F195ECA47DAC";
+char ssid[] = "";
+char pass[] = "";
 int current = 0;
 int interval = 1000;
 
@@ -19,20 +19,20 @@ Servo myservo2;
 
 
 void actionner(Servo test){
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    test.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(5);                       // waits 15ms for the servo to reach the position
+  for (pos = 0; pos <= 180; pos += 1) {
+    test.write(pos);             
+    delay(5);                       
   }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    test.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(5);                       // waits 15ms for the servo to reach the position
+  for (pos = 180; pos >= 0; pos -= 1) { 
+    test.write(pos);              
+    delay(5);                      
   }
 }
 
 
 BLYNK_WRITE(V2)
 {
-  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+  int pinValue = param.asInt(); 
 
   if(pinValue == 1){
     actionner(myservo1);
@@ -45,12 +45,12 @@ BLYNK_WRITE(V2)
 
 void setup()
 {
-  // Debug console
+
   Serial.begin(115200);
   myservo1.attach(D1);
   myservo2.attach(D2);
   Blynk.begin(auth, ssid, pass);
-  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+
 }
 
 void loop()
